04b031f1c73f3901886c3c07c7c752d5103b5ce5
Dom Dom <vaicut6041@gmail.com>
2023-03-02 20:10:04 +0700

    add

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import *
+from gevent.pywsgi import WSGIServer
+
+
+app = Flask(__name__)
+
+
+@app.route("/image/<name>", methods=["GET"])
+def img_route(name):
+    if request.args.get("api", default="", type=str) == "hieu987":
+        src_img = f"./images/{name}"
+        return send_file(
+            src_img,
+            mimetype="image/gif",
+        )
+
+
+if __name__ == "__main__":
+    # app.run(port=7777)
+    http_server = WSGIServer(("", 5000), app)
+    http_server.serve_forever()
