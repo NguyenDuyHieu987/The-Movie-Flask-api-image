a53464ab03d3f407cb55ffd8f9a1fe0325639262
Dom Dom <vaicut6041@gmail.com>
2023-03-03 01:19:35 +0700

    add

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ app = Flask(__name__)
 @app.route("/image/<name>", methods=["GET"])
 def img_route(name):
     if request.args.get("api", default="", type=str) == "hieu987":
-        src_img = f"./images/{name}"
+        src_img = f"images/{name}"
         return send_file(
             src_img,
-            mimetype="image/gif",
+            mimetype="image",
         )
+    return {}
 
 
 if __name__ == "__main__":
     # app.run(port=7777)
-    http_server = WSGIServer(("", 5000), app)
+    http_server = WSGIServer(("", 5001), app)
     http_server.serve_forever()
