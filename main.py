1403ccaeae64a38f06068a2fc1af30d9595226e4
Dom Dom <vaicut6041@gmail.com>
2023-03-03 13:44:45 +0700

    add

--- main.py
+++ main.py
@@ -17,6 +17,6 @@ def img_route(name):
 
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=7777)
-    serve(app, host="0.0.0.0", port=7777)
-    # http_server = WSGIServer(("", 5000), app)
-    # http_server.serve_forever()
+
+    http_server = WSGIServer(("", 5000), app)
+    http_server.serve_forever()
