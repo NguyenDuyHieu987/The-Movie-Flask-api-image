1ece089b34e99232993375bda24e10294f061f30
Dom Dom <vaicut6041@gmail.com>
2023-03-03 13:07:29 +0700

    add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import *
 from gevent.pywsgi import WSGIServer
-
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -11,12 +11,12 @@ def img_route(name):
         src_img = f"images/{name}"
         return send_file(
             src_img,
-            mimetype="image",
+            mimetype="image/gif",
         )
-    return {}
 
 
 if __name__ == "__main__":
-    # app.run(port=7777)
-    http_server = WSGIServer(("", 5001), app)
-    http_server.serve_forever()
+    # app.run(host="0.0.0.0", port=7777)
+    serve(app, host="0.0.0.0", port=7777)
+    # http_server = WSGIServer(("", 5000), app)
+    # http_server.serve_forever()
